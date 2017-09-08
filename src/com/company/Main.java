8bd15c9caa6bbf8e0f4bc04f5dc5be23f460e61d
YOUR_PUBLIC_NAME <YOUR_PUBLIC@GIT_EMAIL.COM>
2017-09-07 19:01:11 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int answer = 0;
+
+        answer = 2 + 2;
+        System.out.println("2 + 2 = " + answer);
+
+        answer = 20-10;
+        System.out.println("20 - 10 = " + answer);
+
+        answer = 5 * 5;
+        System.out.println("5 * 5 = " + answer);
+
+        answer = 100 / 10;
+        System.out.println("100 / 10 = " + answer);
+
+        answer = 10 % 3;
+        System.out.println("10 % 3 = " + answer);
+
+
+    }
+}
